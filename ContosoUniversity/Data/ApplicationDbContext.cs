6888cd96cc1006c4528f11858695024f14db2b9d
David <gstelle@gmail.com>
2017-06-08 20:50:21 -0600

    Sorting, filtering and paging

---
+++ ContosoUniversity/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Course>().ToTable("Course");
+            builder.Entity<Enrollment>().ToTable("Enrollment");
+            builder.Entity<Student>().ToTable("Student");
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage ="Get a shorter last name please. Maximum 50 characters.")]
+        [RegularExpression(@"^[A-Z].*$", ErrorMessage ="Last name must with a capital letter.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Get a shorter first name please. Maximum 50 characters.")]
+        [RegularExpression(@"^[A-Z].*$", ErrorMessage = "First name must with a capital letter.")]
+        [Display(Name = "First Mid Name")]
+        public string FirstMidName { get; set; }
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:d}")]
+        [DataType(DataType.Date, ErrorMessage ="Please enter a valid date (MM/DD/YYYY)")]
+        [Display(Name ="Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
